7dfca1548a1b7e3935afb9bdd19d1947da812767
Daniel Bossér <daniel.bosser@liu.se>
2022-10-11 15:55:02 +0200

    adding nix packaging files

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+    
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flakeutils.url = "github:numtide/flake-utils";
+    render-anim.url = "github:jdbosser/render-anim";
+    render-anim.flake = false;
+  };
+
+
+  outputs = { self, nixpkgs, flakeutils, render-anim }: 
+    flakeutils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; 
+        
+        in
+        {
+            devShells.default =  pkgs.mkShell {
+                  buildInputs = [
+                    (pkgs.python3.withPackages (p: [
+                        p.numpy 
+                        p.matplotlib 
+                        p.setuptools
+                        p.scipy 
+                        p.tqdm
+                    ]))
+                    pkgs.pyright
+                  ];
+            };
+
+        }
+    );
+}
