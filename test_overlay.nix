4dd0c697362f948cac13eacae80342c89086bfea
Daniel Bossér <daniel.bosser@liu.se>
2022-10-17 09:02:39 +0200

    WIP on overlay

--- test_overlay.nix
+++ test_overlay.nix
@@ -1,10 +1,24 @@
 let
   pkgs = import <nixpkgs> {};
-  newpkgs = import pkgs.path { overlays = [ (self: super: {
+  newpkgs = import pkgs.path { overlays = [ 
+
+
+  (self: super: {
     python310 = let
       packageOverrides = python-self: python-super: {
         filtc = self.python310.pkgs.callPackage (import ./pack.nix) {};
       };
     in super.python310.override {inherit packageOverrides;};
-  } ) ]; };
-in newpkgs.python310.withPackages (p: [p.filtc])
+  } ) 
+
+
+  (self: super: {
+    python39 = let
+      packageOverrides = python-self: python-super: {
+        filtc = self.python39.pkgs.callPackage (import ./pack.nix) {};
+      };
+    in super.python39.override {inherit packageOverrides;};
+  } ) 
+
+  ]; };
+in newpkgs.python39.withPackages (p: [p.filtc])
